feecefdccb73eaa00cd3cf4279267d3c6b0f4f7a
Nikita <123288888+spyr0xz@users.noreply.github.com>
2024-02-11 19:05:29 +0300

    app router minor adjustments

--- src/app/providers/router/AppRouter.tsx
+++ src/app/providers/router/AppRouter.tsx
@@ -1,9 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import { routeConfig } from "../../../shared/config/routeConfig/routeConfig";
-import { Suspense, useCallback } from "react";
+import { Suspense } from "react";
+
 
 const AppRouter = () => {
-
+  
   return (
     <Routes>
       {Object.values(routeConfig).map(({ path, element, }) => (
